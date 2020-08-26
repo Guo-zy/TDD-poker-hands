77e6dc88446b7938dddc3e237d1c7405dd68dbe3
Guo-zy <674402145@qq.com>
2020-08-26 20:35:44 +0800

    [poker-hands] [Yancy & Alisa] feature : pass should_return_white_wins_with_straight_of_34567_when_calculate_winner_given_black_with_three_of_a_kind_and_white_with_straight_of_34567

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -308,6 +308,19 @@ public class DemoTest {
     Assertions.assertEquals("white wins with straight flush of 3H4H5H6H7H", res);
   }
 
+  @Test
+  public void should_return_white_wins_with_straight_of_34567_when_calculate_winner_given_black_with_three_of_a_kind_and_white_with_straight_of_34567() {
+    //given
+    String black = "2H 2D 2S 9C KD";
+    String white = "3D 4H 5C 6S 7H";
+
+    //when
+    String res = demo.calculateWinner(black, white);
+
+    //then
+    Assertions.assertEquals("white wins with straight of 34567", res);
+  }
+
 }
 
 
