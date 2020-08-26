156610fbdb66b20fc92c2b49b961ef316a3ab076
Guo-zy <674402145@qq.com>
2020-08-26 20:22:13 +0800

    [poker-hands] [Yancy & Alisa] feature : pass should_return_white_wins_with_straight_of_34567_when_calculate_winner_given_black_with_pair_and_white_with_straight_of_34567

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -165,6 +165,19 @@ public class DemoTest {
     Assertions.assertEquals("white wins with three of a kind of 5", res);
   }
 
+  @Test
+  public void should_return_white_wins_with_straight_of_34567_when_calculate_winner_given_black_with_pair_and_white_with_straight_of_34567() {
+    //given
+    String black = "2H 2D 5S 9C KD";
+    String white = "3D 4H 5C 6S 7H";
+
+    //when
+    String res = demo.calculateWinner(black, white);
+
+    //then
+    Assertions.assertEquals("white wins with straight of 34567", res);
+  }
+
 }
 
 
