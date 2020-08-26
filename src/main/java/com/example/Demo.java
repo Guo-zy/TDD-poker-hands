dc8c0ea9cbdfacd3eee49793897baeda9c420fbc
Guo-zy <674402145@qq.com>
2020-08-26 12:35:45 +0800

    [poker-hands] [Yancy & Alisa] test : test should_return_tie_when_comparison_size_given_same_hands_num

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -1,8 +1,36 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo {
 
-    public String run() {
-        return "ABC";
-    }
+  private int black_score = 0;
+  private int white_score = 0;
+
+  private Map<String, Integer> poker_hands_situation;
+
+  {
+    poker_hands_situation = new HashMap<>();
+    poker_hands_situation.put("High Card", 1);
+    poker_hands_situation.put("Pair", 2);
+    poker_hands_situation.put("Two Pairs", 3);
+    poker_hands_situation.put("Three of a Kind", 4);
+    poker_hands_situation.put("Straight", 5);
+    poker_hands_situation.put("Flush", 6);
+    poker_hands_situation.put("Full House", 7);
+    poker_hands_situation.put("Four of a Kind", 8);
+    poker_hands_situation.put("Straight flush", 9);
+  }
+
+
+  public String run() {
+    return "ABC";
+  }
+
+  public String calculate_winner(String black, String white) {
+
+      return null;
+  }
+
 }
--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -1,21 +1,38 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DemoTest {
-    private static Demo demo;
 
-    @BeforeAll
-    public static void setUp() throws Exception {
-        demo = new Demo();
-    }
+  private static Demo demo;
 
-    @Test
-    public void test01() {
-        String result = demo.run();
-        assertThat(result).isEqualTo("ABC");
-    }
+  @BeforeAll
+  public static void setUp() throws Exception {
+    demo = new Demo();
+  }
+
+  @Test
+  public void test01() {
+    String result = demo.run();
+    assertThat(result).isEqualTo("ABC");
+  }
+
+
+  @Test
+  public void should_return_tie_when_comparison_size_given_same_hands_num() {
+    //given
+    String black = "2H 3D 5S 9C KD";
+    String white = "2D 3H 5C 9S KH";
+
+    //when
+    String res = demo. calculate_winner(black, white);
+
+    //then
+//        assertThat(res).isEqualTo("Tie");
+    Assertions.assertEquals("Tie", res);
+  }
 }
